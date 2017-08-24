cc70caf492a15d9e3b256c6a0a6b1f3459a479ed
Mateusz Grabarski <mateusz.grabarski@gmil.com>
2017-08-24 10:36:14 +0200

    added some fields to product

---
+++ app/src/main/java/grabarski/shoppingcartapp/data/model/LineItem.java
@@ -0,0 +1,10 @@
+package grabarski.shoppingcartapp.data.model;
+
+/**
+ * Created by Mateusz Grabarski on 23/08/2017.
+ */
+
+public class LineItem extends Product {
+
+    private Product quantity;
+}
